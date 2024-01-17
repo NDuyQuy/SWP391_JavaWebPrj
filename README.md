fde341262f2a4da029c1a3b1d64206f9ef443fe8
Quy Nguyen Duy <duyquy140903@gmail.com>
2024-01-17 11:13:37 +0700

    Project Init Version

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
+#SWP391_JavaWebPrj_HandMadePurchasePlatform
+
+# Platform support buy and sell hand-made
+
+#App Use
++ NET BEAN IDE 8.2
++ jdk 1.8
++ jre 1.8
++ Tomcat Server 8.5.93
++ jdbc 4-2.0
++ taglibs-standard-(compat|impl|jstlel|spec) 1.2.5
+
+#Database(SQL 2019)
+
 # SWP391_JavaWebPrj
---
+++ SWP391_JavaWebAppFinalPrj/src/java/dao/SQLConnection.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SQLConnection {
+    public static Connection getConnection() throws ClassNotFoundException, SQLException{
+        Connection con = null;
+        // sign in driver
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        // url = jdbc:sqlserver:// (+ SERVER NAME) : (+PORT); databaseName="(+DATABASENAME)";
+        String url ="jdbc:sqlserver://localhost:1433;databaseName=SWP391_FinalPrjDB;";
+        //intergratedSecurity=true (if connect with SQL Server by Window Authentication)
+        // 
+        String user = "sa";String pass = "123456";
+        con = DriverManager.getConnection(url, user, pass);
+        return con;
+    }
+    public static void main(String[] args) {
+        try {
+            Connection con = getConnection();
+            if(con!=null) System.out.println("Login_Success!");
+            else System.out.println("Failed!");
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(SQLConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
